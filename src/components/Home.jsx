883d96a22a758f456a92bd313cb554540968c235
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-04-30 21:40:20 +0100

    this commit will add functionality to display projects in a grid style and add CSS to it. It will also change the font throughout the website

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1 className='homePageTitle'>Hi, I'm Harriet.</h1>
+      <h2>I'm a full stack developer.</h2>
     </div>
   )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from 'react'
+import ButterflyApp2 from '../Images/ButterflyApp2.png'
+import NCNews from '../Images/NCNews.png'
+import './Portfolio.css'
 
 const Portfolio = () => {
   return (
     <div>
-      <h1>Portfolio</h1>
-    </div>
-  );
-};
+      <h1 className='portfolioTitle'>Portfolio</h1>
+      <ul className='portfolioProjectsList'>
+        <li className='butterflyList'>
+          <h2>Butterfly Hunt Web AR App</h2>
+          <img
+            src={ButterflyApp2}
+            alt='butterfly app'
+            className='butterflyImg'
+          />
+          <p className='butterflyInfo'>
+            Buterfly Hunt is a marker based Augmented Reality game. It is made
+            up of a single page application built in Vue, which connects to a
+            seperately hosted Web-AR application, with bespoke 3D butterflies
+            made in Blender.
+          </p>
+        </li>
 
-export default Portfolio;
+        <li className='ncNewsList'>
+          <h2>NC News</h2>
+          <img src={NCNews} alt='ncNews' className='ncNewsImg' />
+          <p className='ncNewsInfo'>
+            Northcoders News serves as a social news aggregation website where
+            users can rate web content and have discussions in the style of
+            Reddit.
+          </p>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Portfolio
