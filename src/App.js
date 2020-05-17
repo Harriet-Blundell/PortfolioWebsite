7a300f5041c8762ddf594364c5cda8a06e4c3416
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-17 22:13:30 +0100

    change app.js to a functional component

--- src/App.js
+++ src/App.js
@@ -6,27 +6,23 @@ import Contact from './components/Contact'
 import { Router } from '@reach/router'
 import NavBar from './components/NavBar'
 
-class App extends React.Component {
-  state = {
-    navBarItems: [
-      { name: 'home', label: 'HARRIET BLUNDELL', link: '/' },
-      { name: 'portoflio', label: 'PORTFOLIO', link: '/portfolio' },
-      { name: 'contact', label: 'CONTACT', link: '/contact' },
-    ],
-  }
+const App = () => {
+  const navBarItems = [
+    { name: 'home', label: 'HARRIET BLUNDELL', link: '/' },
+    { name: 'portoflio', label: 'PORTFOLIO', link: '/portfolio' },
+    { name: 'contact', label: 'CONTACT', link: '/contact' },
+  ]
 
-  render() {
-    return (
-      <div className='App'>
-        <NavBar sideBarItems={this.state.navBarItems} />
-        <Router className='routeDiv'>
-          <Home path='/' />
-          <Portfolio path='portfolio' />
-          <Contact path='/contact' />
-        </Router>
-      </div>
-    )
-  }
+  return (
+    <div className='App'>
+      <NavBar sideBarItems={navBarItems} />
+      <Router className='routeDiv'>
+        <Home path='/' />
+        <Portfolio path='portfolio' />
+        <Contact path='/contact' />
+      </Router>
+    </div>
+  )
 }
 
 export default App
